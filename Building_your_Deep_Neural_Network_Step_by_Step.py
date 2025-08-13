19261bca8273701e9babe51284e7b2de36c028cd
Muhammad Ubaidullah <148792600+ubirex999@users.noreply.github.com>
2025-08-13 13:59:18 +0500

    Update Building_your_Deep_Neural_Network_Step_by_Step.py

--- Building_your_Deep_Neural_Network_Step_by_Step.py
+++ Building_your_Deep_Neural_Network_Step_by_Step.py
@@ -372,18 +372,8 @@ L_model_backward_test(L_model_backward)
 
 
 def update_parameters(params, grads, learning_rate):
-    """
-    Update parameters using gradient descent
-    
-    Arguments:
-    params -- python dictionary containing your parameters 
-    grads -- python dictionary containing your gradients, output of L_model_backward
-    
-    Returns:
-    parameters -- python dictionary containing your updated parameters 
-                  parameters["W" + str(l)] = ... 
-                  parameters["b" + str(l)] = ...
-    """
+   
+  
     parameters = copy.deepcopy(params)
     L = len(parameters) // 2 # number of layers in the neural network
 
